940709fee3d3bb78574b5a401afe79cebb88aacc
Ashik Mahmud <ashikmahmud@Ashiks-MacBook-Pro.local>
2021-07-28 21:09:05 +0600

    Setup typeorm and connect with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'ashik',
+      database: 'nestjs-playground',
+      entities: [],
+      synchronize: true,
+      logging: true
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
