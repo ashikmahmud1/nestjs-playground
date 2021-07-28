bb790d08b5e9cd6dae448370f41a720d1532d81f
Ashik Mahmud <ashikmahmud@Ashiks-MacBook-Pro.local>
2021-07-28 11:21:00 +0600

    Entity setup

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,10 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
 
-@Resolver()
+@Resolver(of => Restaurant)
 export class RestaurantResolver {
-
-    @Query(returns => Boolean)
-    isPizzaGood(): Boolean {
-        return true
-    }
+  @Query((returns) => [Restaurant])
+  restaurants(): Restaurant[]{
+    return [];
+  }
 }
