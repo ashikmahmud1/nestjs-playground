9822404001387e94da9ec2b535bce9f0231d49d2
Ashik Mahmud <ashikmahmud@Ashiks-MacBook-Pro.local>
2021-08-23 12:50:14 +0600

    Repository pattern for finding and creating record also dto modification

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,8 +1,10 @@
 // Entity is something like model
 
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@InputType({isAbstract: true})
 @ObjectType()
 @Entity()
 export class Restaurant {
@@ -12,22 +14,19 @@ export class Restaurant {
 
   @Field(type => String)
   @Column()
+  @IsString()
+  @Length(5)
   name: string;
 
-  @Field(type => Boolean, { nullable: true })
-  @Column()
+  @Field(type => Boolean, { defaultValue: true }) // for graphql
+  @Column({default: true}) // for database
+  @IsOptional()
+  @IsBoolean()
   isVegan?: boolean;
 
   @Field(type => String)
   @Column()
+  @IsString()
   address: string;
-
-  @Field(type => String)
-  @Column()
-  ownersName: string
-
-  @Field(type => String)
-  @Column()
-  categoryName: string
 
 }
---
+++ src/restaurants/entities/restaurants.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateRestaurantDto } from '../dtos/create-restaurant.dto';
+import { Restaurant } from './restaurant.entity';
+
+@Injectable()
+export class RestaurantService {
+  constructor(
+    @InjectRepository(Restaurant)
+    private readonly restaurants: Repository<Restaurant>,
+  ) {}
+  // access the database
+  getAll(): Promise<Restaurant[]> {
+    return this.restaurants.find();
+  }
+
+  createRestaurant(createRestaurantDto: CreateRestaurantDto): Promise<Restaurant>{
+      // difference between create and save
+      const newRestaurant = this.restaurants.create(createRestaurantDto);
+
+      return this.restaurants.save(newRestaurant)
+  }
+}
